8b73147dde3bac635f8c36170b4a66041c4b0913
Daniel Rodriguez III <DRIIIGistus@gmail.com>
2026-01-16 20:26:48 -0600

    Add caller_region to contact_center table
    Added caller_region to capture regional data.

--- sql/01_DDL_Table_Creation.sql
+++ sql/01_DDL_Table_Creation.sql
@@ -1,11 +1,11 @@
-
--- DDL: Creating the landing table in BigQuery
+-- DDL: Creating the landing table in BigQuery project driiiportfolio
 CREATE OR REPLACE TABLE `driiiportfolio.contact_center.raw_ingestion` (
     call_id STRING,
     source_system STRING,
-    call_timestamp STRING, -- Initially string to handle inconsistent formats
+    call_timestamp STRING, 
     wait_time_sec FLOAT64,
     resolution_status STRING,
     agent_id STRING,
-    caller_intent STRING
+    caller_intent STRING,
+    caller_region STRING -- NEW: Added to capture regional data
 );
